2ff58e0c0da1c4264e656b0bb8f1bfb31c995134
elieceruiz <69103366+elieceruiz@users.noreply.github.com>
2026-03-10 00:06:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,11 +14,13 @@ archivo = st.file_uploader("Sube factura XML")
 if archivo:
 
     items, fecha = leer_factura(archivo)
+
     st.write("Fecha factura:", fecha)
 
     categorias = traer_categorias()
 
     mapa_cat = {c["nombre"]: c["id"] for c in categorias}
+    nombres_cat = list(mapa_cat.keys())
 
     st.subheader("Items detectados")
 
@@ -34,14 +36,21 @@ if archivo:
         if memoria:
             categoria_default = memoria["categoria"]
         else:
-            categoria_default = list(mapa_cat.keys())[0]
+            categoria_default = nombres_cat[0]
 
-        categoria = st.selectbox(
-            producto,
-            mapa_cat.keys(),
-            index=list(mapa_cat.keys()).index(categoria_default)
-            if categoria_default in mapa_cat else 0
-        )
+        col1, col2 = st.columns([4,1])
+
+        with col1:
+
+            categoria = st.selectbox(
+                f"{producto}",
+                nombres_cat,
+                index=nombres_cat.index(categoria_default)
+                if categoria_default in nombres_cat else 0
+            )
+
+        with col2:
+            st.write(f"${precio:,.0f}")
 
         seleccion.append({
             "producto": producto,
