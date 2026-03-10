016af94a1ec55de93e89f5736c62b4ba0edcfb39
elieceruiz <69103366+elieceruiz@users.noreply.github.com>
2026-03-10 04:52:04 -0500

    Update gmail_fetch.py

--- gmail_fetch.py
+++ gmail_fetch.py
@@ -77,7 +77,7 @@ def obtener_adjuntos(service):
 
     results = service.users().messages().list(
         userId="me",
-        q="has:attachment filename:(xml OR zip) subject:(factura OR invoice) newer_than:90d"
+        q="has:attachment filename:(xml OR zip) subject:(factura OR invoice) newer_than:30d"
     ).execute()
 
     mensajes = results.get("messages", [])
